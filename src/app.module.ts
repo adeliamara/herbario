ee34a02d5435d6f4647effa62b41f2b05d630a07
Adélia Mara <adeliamara13@gmail.com>
2023-09-23 23:32:50 -0300

    feat: add new modules to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { FamiliesModule } from './families/families.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './ormconfig'; 
+import { SpeciesModule } from './species/species.module';
+import { GenusModule } from './genus/genus.module';
+import { EnvironmentsModule } from './environments/environments.module';
+import { CollectorsModule } from './collectors/collectors.module';
+import { LocationsModule } from './locations/locations.module';
+import { ExsiccataModule } from './exsiccata/exsiccata.module';
+import { ExsiccataFamilyModule } from './exsiccata-family/exsiccata-family.module';
 
 @Module({
-  imports: [ TypeOrmModule.forRoot(config), FamiliesModule],
+  imports: [ TypeOrmModule.forRoot(config), FamiliesModule, SpeciesModule, GenusModule, EnvironmentsModule, CollectorsModule, LocationsModule, ExsiccataModule, ExsiccataFamilyModule],
   controllers: [AppController],
   providers: [AppService],
 })
