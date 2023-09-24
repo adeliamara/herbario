deb130cb12f4b0ebac13a5d28d8d54c6803c9996
Adélia Mara <adeliamara13@gmail.com>
2023-09-24 11:55:40 -0300

    refactor: remove unnecessary modules

--- src/exsiccata/exsiccata.module.ts
+++ src/exsiccata/exsiccata.module.ts
@@ -1,19 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ExsiccataService } from './exsiccata.service';
 import { ExsiccataController } from './exsiccata.controller';
-import { ExsiccataFamily } from 'src/exsiccata-family/entities/exsiccata-family.entity';
 import { FamiliesService } from 'src/families/families.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Exsiccata } from './entities/exsiccata.entity';
 import { Family } from 'src/families/entities/family.entity';
-import { ExsiccataFamilyService } from 'src/exsiccata-family/exsiccata-family.service';
+import { Genus } from 'src/genus/entities/genus.entity';
 import { Species } from 'src/species/entities/species.entity';
-import { ExsiccataSpeciesService } from 'src/exsiccata-species/exsiccata-species.service';
 import { SpeciesService } from 'src/species/species.service';
+import { GenusService } from 'src/genus/genus.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Exsiccata,ExsiccataFamily,Family, Species])],
+  imports: [TypeOrmModule.forFeature([Exsiccata,Family, Species, Genus])],
   controllers: [ExsiccataController],
-  providers: [ExsiccataService,FamiliesService, ExsiccataFamilyService, ExsiccataSpeciesService, SpeciesService]
+  providers: [ExsiccataService,FamiliesService, SpeciesService, GenusService]
 })
 export class ExsiccataModule {}
