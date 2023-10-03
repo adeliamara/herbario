663eadbbb1b2df1b458897101713dc00c6fdb9ee
Adélia Mara <adeliamara13@gmail.com>
2023-10-02 21:15:43 -0300

    fix: changes ormconfig

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,13 +1,16 @@
 import { DataSourceOptions } from 'typeorm';
 
 export const config: DataSourceOptions = {
-  "type": "postgres",
-  "host": process.env.HOST,
-  "port": 6543,
-  "username": process.env.USERNAME,
-  "password": process.env.PASSWORD,
-  "database": process.env.DATABASE,
-  "entities": ["dist/**/*.entity{.ts,.js}"],
-  "synchronize": true
-}
-
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: process.env.NODE_ENV === 'development',
+  logging: process.env.NODE_ENV === 'development',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  extra: {
+    ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined,
+  },
+};
