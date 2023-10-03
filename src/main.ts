5f2fb702d5b0bb177146605a02fd10f8a6446426
adeliamara <adeliamara13@gmail.com>
2023-10-03 07:03:00 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port, "0.0.0.0");
 }
 bootstrap();
