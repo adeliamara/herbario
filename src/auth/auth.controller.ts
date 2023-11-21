181e14a7179a3f4360ba4991a8492a6f7ed1e485
adeliamara <adeliamara13@gmail.com>
2023-11-20 23:24:52 -0300

    feat: add refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { User } from '../users/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,11 @@ export class AuthController {
   @Post('login')
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('refresh')
+  reauthenticate(@Body() body: string) {
+    return this.authService.reauthenticate(body); 
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,86 @@
-
-import { Injectable} from '@nestjs/common';
-import { UsersService } from '../users/users.service';
+import { Injectable, HttpStatus, HttpException, NotFoundException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UsersService,
-    private jwtService: JwtService
-  ) {}
+    private readonly usersService: UsersService,
+    private jwtService: JwtService,
+  ) { }
 
-  async signIn(email, pass) {
-    console.log(email)
-    const user = await this.usersService.findOne(email);
-    console.log(user)
-    if (user?.password !== pass) {
-      throw new HttpException('Unauthorized', HttpStatus.UNAUTHORIZED);
-      ;
+  async signIn(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findOneByEmail(username);
+
+    if (!user) {
+      throw new HttpException('Usuário ou senha inválidos', HttpStatus.UNAUTHORIZED);
     }
 
-    const payload = { sub: user.id, username: user.email, roles: user.roles};
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
+    console.log(user);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      return await this.generateTokens(user);
+    }
+
+    throw new HttpException('Usuário ou senha inválidos', HttpStatus.UNAUTHORIZED);
+  }
+
+  async generateTokens(payload: User) {
+    const accessToken = this.jwtService.sign(
+      { email: payload.email, id: payload.id, roles: payload.roles },
+      {
+        secret: process.env.SECRET_KEY,
+        expiresIn: '3000s',
+      },
+    );
+
+    const refreshToken = this.jwtService.sign(
+      {email: payload.email, id: payload.id, roles: payload.roles  },
+      {
+        secret: process.env.SECRET_KEY_REFRESH,
+        expiresIn: '7d',
+      },
+    );
+    return { access_token: accessToken, refresh_token: refreshToken };
+  }
+
+  async reauthenticate(refreshToken: string): Promise<any> {
+    const payload: User = await this.verifyRefreshToken(refreshToken);
+    return this.generateTokens(payload);
+  }
+
+  private async verifyRefreshToken(refreshToken: string): Promise<User> {
+    if (!refreshToken) {
+      throw new NotFoundException('Refresh token não fornecido');
+    }
+
+    try {
+      const { email } = this.jwtService.decode(refreshToken) as { email: string };
+      const user: User = await this.usersService.findOneByEmail(email);
+
+      if (!user) {
+        throw new NotFoundException('Usuário não encontrado');
+      }
+
+      this.jwtService.verify(refreshToken, {
+        secret: process.env.SECRET_KEY_REFRESH,
+      });
+
+      return user;
+    } catch (err) {
+      if (err instanceof NotFoundException) {
+        throw err; 
+      }
+
+      if (err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
+        throw new HttpException(err.message, HttpStatus.UNAUTHORIZED);
+      }
+
+      throw new HttpException('Erro ao verificar o Refresh Token', HttpStatus.UNAUTHORIZED);
+    }
   }
 }
