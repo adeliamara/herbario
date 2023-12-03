bf8c17a0d01f6b55a7a859068d3a245dc7bf04b2
adeliamara <adeliamara13@gmail.com>
2023-12-03 15:05:58 -0300

    documentation: use swagger

--- src/botanists/dto/create-botanist.dto.ts
+++ src/botanists/dto/create-botanist.dto.ts
@@ -1,13 +1,17 @@
 import { IsEmail, IsNotEmpty, IsOptional } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBotanistDto {
     @IsNotEmpty()
+    @ApiProperty()
     name: string;
     
     @IsEmail({}, { message: 'Invalid email format', groups: ['optional'] })
+    @ApiProperty()
     @IsOptional()    
     email: string;
 
     @IsNotEmpty()
+    @ApiProperty()
     institution: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+  .setTitle('API herbário')
+  .setDescription('Descrição da API do herbário')
+  .setVersion('1.0')
+  .addTag('herbário')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
