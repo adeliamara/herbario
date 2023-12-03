327b90aaae45e94d27d55b0f861cab08e235f2c5
adeliamara <adeliamara13@gmail.com>
2023-12-03 20:32:57 -0300

    fix: add fields in dto

--- src/user-role/dto/create-user-role.dto.ts
+++ src/user-role/dto/create-user-role.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserRoleDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateUserRoleDto {
+    @IsNotEmpty()
+    roleId: number;
+    
+    @IsNotEmpty()
+    userId: number;
+}
--- src/user-role/user-role.module.ts
+++ src/user-role/user-role.module.ts
@@ -6,9 +6,10 @@ import { User } from '../users/entities/user.entity';
 import { RoleEntity } from '../role/entities/role.entity';
 import { UserRole } from './entities/user-role.entity';
 import { RoleService } from '../role/role.service';
+import { Role } from '../setup/enums/role.enum';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRole])],
+  imports: [TypeOrmModule.forFeature([UserRole, RoleEntity])],
   controllers: [UserRoleController],
   providers: [UserRoleService, RoleService]
 })
