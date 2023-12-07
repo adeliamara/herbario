f2fe6851b44cafce83593edb8d48a3ffe19088c1
adeliamara <adeliamara13@gmail.com>
2023-12-07 01:16:53 -0300

    feat: use environments variables

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,17 +1,18 @@
 import { DataSourceOptions } from 'typeorm';
+import 'dotenv/config'
 
 export const config: DataSourceOptions = {
   type: 'postgres',
-  host: "db.wcztwuknnuyphmqqqddp.supabase.co", //process.env.DB_HOST_HERBARIO,
-  port:5432, //parseInt(process.env.DB_PORT_HERBARIO, 10),
-  username:"postgres", //process.env.DB_USERNAME_HERBARIO,
-  password:"@9NSy%?&SxTqc*&", //String(process.env.DB_PASSWORD_HERBARIO),
-  database: "postgres",//process.env.DB_NAME_HERBARIO,
-  synchronize: true,//process.env.NODE_ENV_HERBARIO === 'development',
-  logging: true,//process.env.NODE_ENV_HERBARIO === 'development',
+  host: process.env.DB_HOST_HERBARIO,
+  port: Number(process.env.DB_PORT_HERBARIO),
+  username:process.env.DB_USERNAME_HERBARIO,
+  password:String(process.env.DB_PASSWORD_HERBARIO),
+  database: process.env.DB_NAME_HERBARIO,
+  synchronize: process.env.NODE_ENV_HERBARIO === 'development',
+  logging: process.env.NODE_ENV_HERBARIO === 'development',
   entities: ['dist/**/*.entity{.ts,.js}'],
   extra: {
-    ssl: false, //process.env.DB_SSL_HERBARIO === 'true' ? { rejectUnauthorized: false } : undefined,
+    ssl: process.env.DB_SSL_HERBARIO === 'true' ? { rejectUnauthorized: false } : undefined,
   },
 };
 
